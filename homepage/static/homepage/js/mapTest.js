9e8a44b5ebf7ac4701ef3e5328d05ab00ebecb95
90MaJH <90majh@gmail.com>
2019-11-05 21:15:49 +0900

    modify mapTest.js

--- homepage/static/homepage/js/mapTest.js
+++ homepage/static/homepage/js/mapTest.js
@@ -1,9 +1,105 @@
-alert("메롱");
-var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-var options = { //지도를 생성할 때 필요한 기본 옵션
-	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-	level: 3 //지도의 레벨(확대, 축소 정도)
-};
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
 
-var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+// 지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption);
 
+// 주소-좌표 변환 객체를 생성합니다
+var geocoder = new kakao.maps.services.Geocoder();
+
+// 주소로 좌표를 검색합니다
+geocoder.addressSearch('경기도 안양시 동안구 엘에스로91번길 16-39', function(result, status) {
+
+    // 정상적으로 검색이 완료됐으면
+     if (status === kakao.maps.services.Status.OK) {
+
+        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+        // 결과값으로 받은 위치를 마커로 표시합니다
+        var marker = new kakao.maps.Marker({
+            map: map,
+            position: coords
+        });
+
+        // 인포윈도우로 장소에 대한 설명을 표시합니다
+        var infowindow = new kakao.maps.InfoWindow({
+            content: '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>'
+        });
+        infowindow.open(map, marker);
+
+        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+        map.setCenter(coords);
+    }
+})
+
+
+
+
+//////////////////////////////////
+//move event and center position//
+//////////////////////////////////
+
+var markers = [];
+
+kakao.maps.event.addListener(map, 'dragend', function() {
+
+    // 지도 중심좌표를 얻어옵니다
+    var latlng = map.getCenter();
+
+    var message = '변경된 지도 중심좌표는 ' + latlng.getLat() + ' 이고, ';
+    message += '경도는 ' + latlng.getLng() + ' 입니다';
+
+
+
+
+
+    //기존에 있던 marker 삭제
+    hideMarkers();
+    //드래그한 후의 중심좌표로 마커 생성
+    addMarker(new kakao.maps.LatLng(latlng.getLat(), latlng.getLng()));
+
+
+
+
+});
+
+
+
+
+
+// 마커를 생성하고 지도위에 표시하는 함수입니다
+function addMarker(position) {
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        position: position
+    });
+
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
+
+    marker.setDraggable(true);
+
+    // 생성된 마커를 배열에 추가합니다
+    markers.push(marker);
+}
+
+// 배열에 추가된 마커들을 지도에 표시하거나 삭제하는 함수입니다
+function setMarkers(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+// "마커 보이기" 버튼을 클릭하면 호출되어 배열에 추가된 마커를 지도에 표시하는 함수입니다
+function showMarkers() {
+    setMarkers(map)
+}
+
+// "마커 감추기" 버튼을 클릭하면 호출되어 배열에 추가된 마커를 지도에서 삭제하는 함수입니다
+function hideMarkers() {
+    setMarkers(null);
+}
